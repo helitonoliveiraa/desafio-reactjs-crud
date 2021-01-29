31317a47537e872ab95643a46f6494fe409b6900
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-01-29 11:23:39 -0300

    :sparkles: feat: loading data from API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,9 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get('foods');
+
+      setFoods(response.data);
     }
 
     loadFoods();
