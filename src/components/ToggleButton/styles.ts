f4a45be2b44ecae3d1cd20bec27467ce1c6cd37d
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-02-01 14:35:49 -0300

    :sparkles: feat: toggle button to switch themes

---
+++ src/components/ToggleButton/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  background: none;
+
+  margin-right: 4.6rem;
+
+  svg {
+    color: '#FFF';
+  }
+`;
